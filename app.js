67bf101223ed286b345276ce2971be16a5bd9b9b
Gil Cohen <gilc@copyleaks.com>
2022-08-09 18:51:03 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.static());
+app.set('view engine', 'ejs');
+
+// app.use(express.json()); // exept jaon data from user
+
+// const templatesPath = path.join(__dirname, "./views/");
+// app.set("views", templatesPath);
+
+// let isTrue = false;
+ app.get('/', (req, res) => {
+     res.render("index")
+ })
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`http://localhost:${PORT}`);
+})
